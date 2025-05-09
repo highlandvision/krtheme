9bcb25db5e6b92c3bf1f935b92d889b3eec1011c
Hazel Wilson <hazel@highlandvision.com>
2025-05-09 17:13:50 +0100

    v51-Prepare and Test Release

--- component.php
+++ component.php
@@ -16,11 +16,11 @@ use Joomla\CMS\Factory;
 /** @noinspection PhpUnhandledExceptionInspection */
 $app = Factory::getApplication();
 
-$wa  = $this->getWebAssetManager();
+$wa = $this->getWebAssetManager();
 $wa->usePreset('template.krtheme.site');
 
 $color = $this->params->get('colors', 'colors_default');
-$asset  = 'theme.' . $color;
+$asset = 'theme.' . $color;
 $wa->registerAndUseStyle($asset, 'media/templates/site/krtheme/css/global/' . $color . '.css');
 $wa->getAsset('style', 'fontawesome')->setAttribute('rel', 'lazy-stylesheet');
 ?>
@@ -28,13 +28,13 @@ $wa->getAsset('style', 'fontawesome')->setAttribute('rel', 'lazy-stylesheet');
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>">
 <head>
-	<jdoc:include type="metas" />
+	<jdoc:include type="metas"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<jdoc:include type="styles" />
-	<jdoc:include type="scripts" />
+	<jdoc:include type="styles"/>
+	<jdoc:include type="scripts"/>
 </head>
 <body class="<?php echo $this->direction === 'rtl' ? 'rtl' : ''; ?>">
-<jdoc:include type="message" />
-<jdoc:include type="component" />
+<jdoc:include type="message"/>
+<jdoc:include type="component"/>
 </body>
 </html>
